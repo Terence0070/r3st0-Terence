83657ce480b92a2ddc83c2469387d7a6158bd6d5
Terence0070 <terence.renard@gmail.com>
2023-12-14 17:11:29 +0100

    TP 3 terminé

--- r3st0 TP3 EN COURS/site/vue/vueMonProfil.php
+++ r3st0 TP3 EN COURS/site/vue/vueMonProfil.php
@@ -7,10 +7,12 @@ Mon pseudo : <?= $util["pseudoU"] ?> <br />
 <hr>
 
 les restaurants que j'aime : <br>
-    <a href="./?action=detail&idR=4">Cidrerie du fronton</a><br>
-    <a href="./?action=detail&idR=6">Le Bistrot Sainte Cluque</a><br>
-    <a href="./?action=detail&idR=8">La table de POTTOKA</a><br>
+    <?php for ($i = 0; $i < count($mesRestosAimes); $i++) { ?>
+        <?php echo '<a href="./?action=detail&idR=' .  $mesRestosAimes[$i]["idR"] . '">' . $mesRestosAimes[$i]["nomR"] . '</a><br>'; ?>
+    <?php } ?>
+    
 <hr>
+
 les types de cuisine que j'aime : 
 <ul id="tagFood">		
     <?php for ($j = 0; $j < count($mesTypeCuisineAimes); $j++) { ?>
